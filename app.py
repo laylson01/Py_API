8e6cf8a3d5a6c87d459af28cab47593dc70ed95d
laylson01 <laylson01@hotmail.com>
2024-08-05 15:03:15 +0100

    added consult function

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-music = [
+musics = [
     { 
    "id": 1,
    "musica" : "Carry On",
@@ -22,10 +22,23 @@ music = [
 },  
 ]
 
-
 #Função Get/Obter.
-@app.route ("/music")
-def obt_music ():
-   return jsonify(music)
 
-app.run(port=4000, host="localhost", debug=True)
+@app.route ("/musics", methods=["GET"]) #Rota #Metodo (GET)
+def obt_music ():
+   return jsonify(musics)
+
+#Função Consultar p/ID
+@app.route("/musics/<int:id>",methods=["GET"] )
+
+def consult_music_id(id):
+   for musica in musics:
+      if musica.get ("id") == id:
+       return jsonify(musica)
+
+
+
+
+
+
+app.run(port=4000, host="localhost", debug=True) #Config para acesssar localmente.
