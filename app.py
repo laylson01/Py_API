77776ce055e3a794d71d89551abe22112d23eae1
laylson01 <laylson01@hotmail.com>
2024-08-05 13:28:57 +0100

    git init

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+musica = [
+    
+    { 
+   "id": 1,
+   "musica" : "Carry On",
+   "cantor" : "Angra",
+},
+
+{ 
+
+   "id": 2,
+   "musica" : "Fairy Tail",
+   "cantor" : "André Matos",
+},
+
+
+{   
+
+   "id": 3,
+   "musica" : "Moonlight",
+   "cantor" : "Víper",
+},
+
+]
